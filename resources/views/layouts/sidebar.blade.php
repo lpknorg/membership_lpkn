574ac9d9e2437656075f0188c03d6e2683dd121c
Rusby <ianrusbiy02@gmail.com>
2023-07-24 09:22:23 +0700

    siedupdate

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -30,7 +30,7 @@
               <li><a href="{{route('admin.kota.index')}}">Kota</a></li>
               <li><a href="{{route('admin.instansi.index')}}">Instansi</a></li>
               <li><a href="{{route('admin.lembaga_pemerintahan.index')}}">Lembaga Pemerintahan</a></li>
-              <li><a href="{{route('admin.kategori_tempat_kerja.index')}}"><i class="fa fa-building"></i>Kategori Tempat Kerja<span class="label label-success pull-right"></span></a></li>
+              <li><a href="{{route('admin.kategori_tempat_kerja.index')}}">Kategori Tempat Kerja</a></li>
             </ul>
           </li>
           <li><a href="{{route('admin.user.index')}}"><i class="fa fa-user"></i>User<span class="label label-success pull-right"></span></a></li>
