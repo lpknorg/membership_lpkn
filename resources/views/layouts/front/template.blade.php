7fae4fbe1edccae0d30b39324c1592bf4d727b5d
Rusby <ianrusbiy02@gmail.com>
2023-08-01 16:46:53 +0700

    jquery

--- resources/views/layouts/front/template.blade.php
+++ resources/views/layouts/front/template.blade.php
@@ -29,7 +29,6 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
 	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
 	@yield('scripts')
 </body>
