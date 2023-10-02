a729cb4e4bed395b326b5740d7fe75145eeb7036
ari ramadhani <ari3ramadhani@gmail.com>
2023-10-02 16:46:40 +0700

    fixed slider gallery

--- resources/views/Frontend/index.blade.php
+++ resources/views/Frontend/index.blade.php
@@ -1,5 +1,39 @@
-@include('template.head')
-<title>LPKN</title>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <!-- start testimoni -->
+        <!-- MAGNIFIC POPUP-->
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css">
+        <!-- jQuery -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+        <!-- DATATABLES BS 4-->
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <!-- BOOTSTRAP 4-->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
+    <!-- end testimoni -->
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('template/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('frontend/css/navbar.css')}}?version=0">
+    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}?version=0">
+    <link rel="stylesheet" href="{{asset('frontend/css/testimoni.css')}}?version=0">
+    <link rel="stylesheet" href="{{asset('frontend/css/slick.css')}}?version=0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend/images/logo_icon.png')}}">
+    <!-- start slick -->
+        {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> --}}
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css"/>
+    <!-- end slick -->
+  </head>
+<body>
 @include('member.layouts.navbar')
 @include('Frontend.slider')
 @include('Frontend.posting.index')
@@ -8,7 +42,19 @@
 @include('Frontend.body.footer')
 <!-- Optional JavaScript; choose one of the two! -->
 <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-@include('template.footer')
+<!-- <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+{{-- start slick --}}
+    {{-- <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script> --}}
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
+{{-- end slick --}}
+
 @include('Frontend.posting.js')
 @include('member.layouts.modals')
 @include('js/custom_script')
@@ -18,7 +64,7 @@
     $('body').on('click', '[id="btnSelengkapnya"]', function(e) {
         let sl = $(this).attr('slug')
         $('#exampleModal').modal('show')
-        getEvent(sl)        
+        getEvent(sl)
     })
     @if(session('key_slug'))
     setTimeout(function() {
---
+++ resources/views/pages/artikel/list_artikel.blade.php
@@ -0,0 +1,75 @@
+<div class="container con_full mb-4">
+    <div class="row mt-2 sm-reverse">
+        <div class="col-md-8 mb-4 mb-md-0">
+            <h1 class="h_artikel">List Artikel</h1>
+            <hr class="h_line mt-0">
+            <form action="">
+                <div class="row align-items-center">
+                    <div class="col-12 col-sm-3 mb-2 mb-md-0">
+                        <input type="text" class="form-control" name="q" placeholder="Cari disini ..." value="{{\Request::get('q')}}">
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <select name="kategori" class="form-control">
+                            <option value="">Pilih Kategori</option>
+                        </select>
+                    </div>
+                    <div class="col-6 col-sm-3">
+                        <select name="tahun" class="form-control">
+                            <option value="">Pilih Tahun</option>
+                        </select>
+                    </div>
+                    <!-- <div class="col">
+                        <select name="kategori" class="form-control">
+                            <option value="">Pilih Bulan</option>
+                        </select>
+                    </div> -->
+                    <div class="col-12 col-sm-3 mt-2 mt-md-0">
+                        <button type="submit" class="btn btn-primary w-100">Tampilkan</button>
+                    </div>
+                </div>
+            </form>
+            @foreach($data as $d)
+            <div class="card mt-3">
+                <div class="row list_artikel_card">
+                    <div class="col-sm-5">
+                        <div class="list_art_out_img">
+                            <img class="list_art_in_img position-relative" src="{{\Helper::showImage($d->cover, 'artikel/cover')}}" alt="">
+                            <div class="position-absolute" style="bottom: 20px;">
+                                <span class="list_artikel_category">
+                                    Kategori Disini
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-7">
+                        <div class="p-2 d-flex align-items-stretch flex-wrap list_artikel_sm_7">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <a class="list_artikel_out"><img class="list_artikel_in" src="https://www.saveseva.com/wp-content/uploads/2015/06/Landscape.jpg" alt="User profile picture"></a>
+                                </div>
+                                <div class="">
+                                    <div class="artikel_short_name px-2">Ini Adalah bsdenar toh kan</div>
+                                    <div class="artikel_short_date px-2">29 September 2023 | 4 menit lalu</div>
+                                </div>
+                            </div>
+                            <div class="">
+                                <a class="list_artikel_title" href="{{route('artikel.detail', ['uname' => \Helper::getUname($d->user) ,'slug' => $d->slug])}}">
+                                    {{$d->judul}}
+                                </a>
+                            </div>
+                            <div class="d-flex align-items-end ml-auto">
+                                <div class="text-67"> <i class="fa-regular fa-eye"></i> 123</div>
+                                <div class="text-67 mx-4"> <i class="fa-solid fa-heart"></i> 456</div>
+                                <div class="text-67"> <i class="fa-solid fa-message"></i> 7890</div>
+                            </div>
+                        </div>
+                        <!-- <a href="#" class="btn btn-primary btn-sm float-right">Read More</a> -->
+                    </div>
+                </div>
+            </div>
+            @endforeach
+            <div class="float-right mt-3">
+                {{$data->links()}}
+            </div>
+    </div>
+</div>
