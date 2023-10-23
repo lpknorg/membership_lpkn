719afc6ce1a964b1c7471f97e22ed01bbd9caa37
ari ramadhani <ari3ramadhani@gmail.com>
2023-10-23 16:50:55 +0700

    notif

---
+++ resources/views/notification.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.front.template')
+@section('content')
+<div class="container mt-4">
+    <div class="">
+        <h1 class="text-center"> Notifikasi</h1>
+        <div class="dropdown-divider m-0"></div>
+        <a href="" class="dropdown-item notif_before_read">
+            <span>asdasd asd asd asdasdsdsad asdas dasd</span> <br>
+            <p class="m-0 text-right small">2023-08-14 13:48:09</p>
+        </a>
+        <div class="dropdown-divider m-0"></div>
+        <a href="" class="dropdown-item notif_before_read">
+            <span>Ini belum dibaca</span> <br>
+            <p class="m-0 text-right small">2023-08-14 13:48:09</p>
+        </a>
+        <div class="dropdown-divider m-0"></div>
+        <a href="" class="dropdown-item notif_before_read">
+            <span>Ini belum dibaca</span> <br>
+            <p class="m-0 text-right small">2023-08-14 13:48:09</p>
+        </a>
+        <div class="dropdown-divider m-0"></div>
+        <a href="" class="dropdown-item">
+            <span>Ini belum dibaca</span> <br>
+            <p class="m-0 text-right small">2023-08-14 13:48:09</p>
+        </a>
+        <div class="dropdown-divider m-0"></div>
+        <a href="" class="dropdown-item">
+            <span>Ini sudah dibaca</span> <br>
+            <p class="m-0 text-right small">2023-08-14 13:48:09</p>
+        </a>
+    </div>
+</div>
+@endsection
+
